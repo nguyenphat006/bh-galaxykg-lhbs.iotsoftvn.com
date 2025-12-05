b3c158550128f0a277fff3fd521acc768e6181c6
Nguyen Dang Phat <nguyendangphat1505@gmail.com>
2025-12-05 23:02:52 +0700

    Init

---
+++ app/components/home-page/index.ts
@@ -0,0 +1,26 @@
+import { EducationPillarsSection, EducationPillarModal } from '@/components/home-page/section/education-pillar';
+import VideoHero from './hero-video'
+import HeroCarousel from './hero-carousel'
+import FoundingMessageSection from './section/founding-message'
+import { CoreStrengthsSection, CoreStrengthModal } from './section/core-strength'
+import NewsEventsSection from './section/news-events'
+import TestimonialsSection from './section/testimonial'
+import TestimonialQuoteSection from './section/testimonial-quote'
+import { CollegeAcceptancesSection } from './section/college-acceptances'
+import TheNumbers from './section/the-numbers'
+import EducationLevel from './section/education-level'
+export { 
+  VideoHero, 
+  HeroCarousel, 
+  FoundingMessageSection, 
+  CoreStrengthsSection, 
+  CoreStrengthModal,
+  NewsEventsSection,
+  TestimonialsSection,
+  CollegeAcceptancesSection,
+  EducationPillarModal,
+  EducationPillarsSection,
+  TestimonialQuoteSection,
+  TheNumbers,
+  EducationLevel
+}
